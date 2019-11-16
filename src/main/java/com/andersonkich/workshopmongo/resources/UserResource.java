52563f794f533121cc3045e53329a19308aa8288
Anderson Kich <kich42@hotmail.com>
2019-11-16 07:15:23 -0300

    Conectando ao MongoDB com repository e service

--- src/main/java/com/andersonkich/workshopmongo/resources/UserResource.java
+++ src/main/java/com/andersonkich/workshopmongo/resources/UserResource.java
@@ -1,30 +1,27 @@
-package com.andersonkich.workshopmongo.resource;
+package com.andersonkich.workshopmongo.resources;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.andersonkich.workshopmongo.domain.User;
+import com.andersonkich.workshopmongo.services.UserService;
 
 @RestController
 @RequestMapping(value = "/Users")
 public class UserResource {
 
+	@Autowired
+	private UserService service;
+	
 	@GetMapping
 	public ResponseEntity<List<User>> findAll(){
 		
-		User maria = new User("1001", "Maria Brown", "maria@gmail.com");
-		User alex = new User("1002", "Alex Green", "alex@gmail.com");
-		
-		List<User> list = new ArrayList<>();
-		list.addAll(Arrays.asList(maria,alex));
-		
+		List<User> list = service.findAll();
 		return ResponseEntity.ok().body(list);
 	}
 	
---
+++ src/main/java/com/andersonkich/workshopmongo/services/UserService.java
@@ -0,0 +1,23 @@
+package com.andersonkich.workshopmongo.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.andersonkich.workshopmongo.domain.User;
+import com.andersonkich.workshopmongo.repositories.UserRepository;
+
+@Service
+public class UserService {
+
+	@Autowired
+	private UserRepository repository;
+	
+	
+	public List<User> findAll(){
+		return repository.findAll();
+	}
+	
+	
+}
