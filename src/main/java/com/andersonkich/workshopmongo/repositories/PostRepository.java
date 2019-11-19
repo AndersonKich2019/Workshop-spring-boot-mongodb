ecff4e8dce627527da71c10fedb183425ba0e6bf
Anderson Kich <kich42@hotmail.com>
2019-11-19 09:21:22 -0300

    Consulta com vários critérios

--- src/main/java/com/andersonkich/workshopmongo/repositories/PostRepository.java
+++ src/main/java/com/andersonkich/workshopmongo/repositories/PostRepository.java
@@ -1,5 +1,6 @@
 package com.andersonkich.workshopmongo.repositories;
 
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
@@ -17,5 +18,23 @@ public interface PostRepository extends MongoRepository<Post, String> {
 	//Usando notação @Query para buscas;
 	@Query("{ 'title': { $regex: ?0, $options: 'i' } }")
 	List<Post> searchTitle(String text);
+	
+	@Query("{ $and: [ {'data': {$gte: ?1}}, { 'data': { $lte: ?2} }, { $or: [ { 'title': { $regex: ?0, $options: 'i' } }, { 'body': { $regex: ?0, $options: 'i' } }, { 'list.text': { $regex: ?0, $options: 'i' } } ] } ] }")
+	List<Post> fullSearch(String text,Date minDate, Date maxDate);
+	
+	
+	
+	/*Expressões JSon
+	 * expressão ou = { $or: [ { <expression1> }, { <expression2> }, ... , { <expressionN> } ] }
+	 * expressão and = { $and: [ { <expression1> }, { <expression2> } , ... , { <expressionN> } ] }
+	 * expressao >=  = {field: {$gte: value} }
+	 * expressão <=  = { field: { $lte: value} }
+	 * 
+	 */
+	
+	
+	
+	
+	
 	
 }
