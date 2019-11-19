9d9cb97c0524acc9053408680de6c80ea3380f11
Anderson Kich <kich42@hotmail.com>
2019-11-19 07:15:56 -0300

    Consulta simples com query methods

--- src/main/java/com/andersonkich/workshopmongo/domain/Post.java
+++ src/main/java/com/andersonkich/workshopmongo/domain/Post.java
@@ -1,12 +1,15 @@
 package com.andersonkich.workshopmongo.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import com.andersonkich.workshopmongo.dto.AuthorDTO;
+import com.andersonkich.workshopmongo.dto.CommentDTO;
 
 @Document
 public class Post implements Serializable {
@@ -18,6 +21,8 @@ public class Post implements Serializable {
 	private String title;
 	private String body;
 	private AuthorDTO author;
+	
+	private List<CommentDTO> list = new ArrayList<>();
 	
 	public Post() {
 		
@@ -41,6 +46,15 @@ public class Post implements Serializable {
 
 	public Date getData() {
 		return data;
+	}
+	
+
+	public List<CommentDTO> getList() {
+		return list;
+	}
+
+	public void setList(List<CommentDTO> list) {
+		this.list = list;
 	}
 
 	public void setData(Date data) {
